ab36951207575386ce7d4a20629014ed05e9cbcc
Anthony Loria <agloria2001@gmail.com>
2023-05-15 12:17:24 -0600

    Worked on checkbox, changed the implementation to have a pseudo element on top of an input to display custom checkboxs

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,65 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/main.css";
 import styled from "styled-components";
 import { getThemeValue } from "./ThemeProvider";
 
 export type CheckBoxProps = React.HTMLAttributes<HTMLInputElement> & {
-  type: "checkbox";
-  variant?: "outlined" | "contained";
-  accent?: "primary" | "secondary" | "success" | "warning" | "error" | "info";
-  disabled?: boolean;
-  href?: string;
-  shape?: "square" | "rounded";
+  state: boolean
 };
 
 export const CheckBox = (props: CheckBoxProps) => {
-  return <CheckBoxElement {...props} />;
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <label>
+      <InputElement type="checkbox" onChange={() => {
+        setIsChecked(!isChecked);
+      }}/>
+      <CheckBoxElement className={`${isChecked ? "checkbox--active" : ""}`} aria-hidden="true" {...props} />
+    </label>
+  );
 };
 
-const CheckBoxElement = styled.input<CheckBoxProps>`
-  font-weight: 400;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
-  transition: all 0.2s ease-in-out;
-  text-decoration: none;
-  border: ${(props) => {
-    if (props.variant !== "outlined") return "none";
-    return `1px solid ${
-      props.theme[props.accent || "info"] ??
-      getThemeValue(props.accent || "info")
-    }`;
-  }};
-  background-color: ${(props) => {
-    if (props.variant === "contained")
-      return (
-        props.theme[props.accent || "info"] ??
-        getThemeValue(props.accent || "info")
-      );
-    return "transparent";
-  }};
-  color: ${(props) => {
-    if (props.variant === "contained") return "#fff";
-    return (
-      props.theme[props.accent || "info"] ??
-      getThemeValue(props.accent || "info")
-    );
-  }};
+/* taken from https://css-tricks.com/inclusively-hidden/ */
+const CheckBoxElement = styled.svg<CheckBoxProps>`
+  display: inline-block;
+  height: 20px;
+  width: 20px;
+  background: #fff;
+  border: 2px #ddd solid;
+  margin-right: 4px;
 
-  &:disabled {
-    border: ${(props) => {
-      if (props.variant !== "outlined") return "none";
-      return `1px solid ${props.theme.disabled ?? getThemeValue("disabled")}`;
-    }};
-    background-color: ${(props) => {
-      if (props.variant === "contained")
-        return props.theme.disabled ?? getThemeValue("disabled");
-      return "transparent";
-    }};
-    color: ${(props) => {
-      if (props.variant === "contained") return "#fff";
-      return props.theme.disabled ?? getThemeValue("disabled");
-    }};
-  }
+  
+`;
+
+const InputElement = styled.input`
+  position: absolute;
+  opacity: 0;
 `;
