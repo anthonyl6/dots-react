bd7b7e0529902927a81b269ece90d9bc6a739f60
Anthony Loria <agloria2001@gmail.com>
2023-05-17 10:49:49 -0600

    merge conflict with index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ export { Background } from './components/Background';
 export { TextField, type TextFieldProps } from './components/TextField';
 export { Text, type TextProps } from './components/Text';
 export { CheckBox, type CheckBoxProps } from './components/CheckBox';
-export { ThemeProvider } from './components/ThemeProvider'; 
+export { ThemeProvider } from './components/ThemeProvider';
+export { DotsUIComponentRegistry } from './components/next';
